37574df9c42ff1057a718d665144a0ed6f3d4a61
Ben Wang <59347587+Xiaoyu-Ben-Wang@users.noreply.github.com>
2023-01-07 12:39:14 -0700

    server with login and register

---
+++ server/app.py
@@ -0,0 +1,49 @@
+import hashlib
+from db_interface import create_user, login_user
+import os
+from models import db, Course, setup
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token, JWTManager
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(os.getcwd(), 'database.db')
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
+db.init_app(app)
+jwt = JWTManager(app)
+
+
+def gen_login_token(username):
+    create_access_token(identity=username)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json['username']
+    password = request.json['password']
+
+    create_user(username, password)
+
+    return jsonify(success=True), 200
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json['username']
+    password = request.json['password']
+    token = login_user(username, password)
+
+    if token:
+        return jsonify(token=token), 200
+    else:
+        return jsonify(message='Access Denied'), 401
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        setup()
+    app.run(debug=True)
